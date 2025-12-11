0b79db4373d7ab7ee6ccfbb86e299fb5fc7e4336
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-12-11 07:49:50 +0545

    minor changes

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -5,17 +5,19 @@ from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataVa
 from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
+from networksecurity.constants.training_pipeline import SCHEMA_FILE_PATH
+from networksecurity.utils.main_utils.utils import read_yaml_file
 
 from scipy.stats import ks_2samp
 import pandas as pd
 
 class DataValidation:
-    def __init__(self, data_validation_config: DataValidationConfig):
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_config: DataValidationConfig):
         try:
+            self.data_ingestion_artifact = data_ingestion_artifact
             self.data_validation_config = data_validation_config
+            self._schema_config = read_yaml_file(SCHEMA_FILE_PATH)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    
-    
     
